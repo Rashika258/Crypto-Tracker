d59eeb6e94db306f258fa150b2942270490de8c2
Rashika S <rashika.s@codemonk.in>
2021-12-15 11:16:57 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDTk1RuC_iZn9U4DLVgcJXmd--JykpCOy4",
+    authDomain: "crypto-hunter-4875e.firebaseapp.com",
+    projectId: "crypto-hunter-4875e",
+    storageBucket: "crypto-hunter-4875e.appspot.com",
+    messagingSenderId: "21507994695",
+    appId: "1:21507994695:web:5f158a22ceb3c1ad5e6d62"
+  };
+
+export default firebaseConfig;  
